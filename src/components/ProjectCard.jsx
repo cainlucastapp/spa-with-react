d1187d361eced0652ba76f5fd016dc0550eff837
Cain Tapp <cainlucastapp@gmail.com>
2025-11-18 10:24:45 -0800

    display new data

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,7 +8,19 @@ function ProjectCard({ project }) {
         <div className="content">
           <h3>{project.title}</h3>
           <p>{project.description}</p>
+          
+          {project.category && project.category.length > 0 && (
+            <div className="categories">
+              {project.category.map((cat, index) => (
+                <span key={index} className="category-tag">{cat}</span>
+              ))}
+            </div>
+          )}
         </div>
+        
+        {project.releaseDate && (
+          <p className="release-date">Release Date: {new Date(project.releaseDate).toLocaleDateString()}</p>
+        )}
       </div>
     </>
   )
